ae0c4181044409e80c7aeb2db2c013fd8a17ac8e
eumiras <51832486+eumiras@users.noreply.github.com>
2020-12-07 16:06:24 -0300

    Update Cachorro.java

--- projeto_02_java/src/projeto_02_java/Cachorro.java
+++ projeto_02_java/src/projeto_02_java/Cachorro.java
@@ -1,5 +1,3 @@
-package projeto_02_java;
-
 import java.util.Scanner;
 
 public class Cachorro {
@@ -10,16 +8,12 @@ public class Cachorro {
 	int lacinhomax = 10;
 	
 	Cachorro(int idade){
-		this.felicidade = felicidade;
-		this.energia = energia;
 		this.idade = idade;
-		this.lacinhos = lacinhos;
-		this.lacinhomax = lacinhomax;
 	}
 	
 	void brincar () {
 		if(energia == 0) {
-			System.out.println("n„o vai dar n„o minha filha");
+			System.out.println("n√£o vai dar n√£o minha filha");
 			return;
 		}
 		else {
@@ -41,94 +35,85 @@ public class Cachorro {
 	void enfeitar (int lacinho) {
 		if (felicidade == 0) {
 			System.out.println("EU VOU TE MORDEEEE");
-			return;
-		}
-		else if (lacinho > 10){
+		} else if (lacinho > 10){
 			System.out.println("NAAOO CABEEEE");
-			return;
-		}
-		
-		else if ((lacinhos + lacinho) > lacinhomax){
-			System.out.println("n„o tem mais espaÁo!! n„o t· vendo??");
-			return;
-		}
-		
-		else {
-			lacinhos+=lacinho;
-			System.out.println("vou deixar, mas sÛ um hein...");
+		} else if ((this.lacinhos + lacinho) > lacinhomax){
+			System.out.println("n√£o tem mais espa√ßo!! n√£o t√° vendo??");
+		} else {
+			this.lacinhos += lacinho;
+			System.out.println("vou deixar, mas s√≥ um hein...");
 		}
 	}
 	
 	
-	void modoturbo () {
-		if(idade>15) {
-			System.out.println("olha moÁa, n„o tenho mais idade pra isso");
+	void brincarLoucamente () {
+		if(idade > 15) {
+			System.out.println("olha mo√ßa, n√£o tenho mais idade pra isso");
 			return;
 		}
-		else {
-			lacinhos = 0;
-			energia = 0;
-			felicidade +=10;
-			idade +=1;
-			System.out.println("MODO TUUUURBOOOO");
-		}
+		lacinhos = 0;
+		energia = 0;
+		felicidade +=10;
+		idade +=1;
+		System.out.println("MODO TUUUURBOOOO");
 	}
 	
-	void status () {
-		System.out.println("Aqui v„o meus status!");
-		System.out.println("minha energia:" + energia);
-		System.out.println("meu nÌvel de felicidade:" + felicidade);
-		System.out.println("minha idade:" + idade);
-		System.out.println("meus lacinhos:" + lacinhos);
+	public String toString(){
+		String saida = "Aqui v√£o meus status!\n";
+		saida += "minha energia:" + energia + "\n";
+		saida += "meu n√≠vel de felicidade:" + felicidade + "\n";
+		saida += "minha idade:" + idade + "\n";
+		saida += "meus lacinhos:" + lacinhos + "\n";
+		return saida;
 	}
 	
 	//public String toString() {
-	//	return "minha energia:" + energia + "meu nÌvel de felicidade:" + felicidade;
+	//	return "minha energia:" + energia + "meu n√≠vel de felicidade:" + felicidade;
 	//}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Cachorro perola = new Cachorro(1);
 		System.out.println("--- Bem vindo!!---");
-		System.out.println("eu sou PÈrola, uma catiorinea!");
+		System.out.println("eu sou P√©rola, uma catiorinea!");
 		System.out.println("aqui vai o menu <3");
 		System.out.println("Digite o que fazer em seguida:");
 		System.out.println("brincar -  vamos brincar e eu vou ficar bem feliz");
 		System.out.println("descansar (horas) - vou ficar feliz tbm, mas dormindo ");
 		System.out.println("enfeitar - vc coloca lacinhos em mim e eu te mordo ");
-		System.out.println("turbo - o que pode acontecer È um mistÈrio...");
+		System.out.println("turbo - o que pode acontecer √© um mist√©rio...");
 		
 		
 		while(true) {
 			String line = scanner.nextLine();
 			String input[] = line.split(" ");
 			if (input[0].equals("end")) {
-				System.out.println("È isso. acabou. boa sortee...");
+				System.out.println("√© isso. acabou. boa sortee...");
 				break;
 			}
 			
 			else if (input[0].equals("brincar")) {
 				perola.brincar();
-				perola.status();
+				System.out.println(perola);
 			}
 			
 			else if (input[0].equals("descansar")) {
 				int horas = Integer.parseInt(input[1]);
 				perola.descansar(horas);
-				perola.status();
+				System.out.println(perola);
 			}
 			
 			else if (input [0].equals("enfeitar")) {
 				int lacinho = Integer.parseInt(input[1]);
 				perola.enfeitar(lacinho);
-				perola.status();
+				System.out.println(perola);
 			}
 			
 			else if (input [0].equals("turbo")) {
-				perola.modoturbo();
-				perola.status();
+				perola.brincarLoucamente();
+				System.out.println(perola);
 			}
 		}
+		scanner.close();
 	}	
 }
-
